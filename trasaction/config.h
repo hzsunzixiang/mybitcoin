bec4796251877884df8c1d4f7458c1f175c136ae
sunzixiang <haichengsun123@163.com>
2018-09-22 06:33:03 +0800

    move file

--- trasaction/config.h
+++ trasaction/config.h
@@ -55,19 +55,19 @@
 #define LT_OBJDIR ".libs/"
 
 /* Name of package */
-#define PACKAGE "hw"
+#define PACKAGE "src"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT "foo.bar@example.com"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "hw"
+#define PACKAGE_NAME "src"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "hw 1.0.0"
+#define PACKAGE_STRING "src 1.0.0"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "hw"
+#define PACKAGE_TARNAME "src"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL ""
